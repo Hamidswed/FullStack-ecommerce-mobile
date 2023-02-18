727efca6bf6cade89d78657baa79339777ba079c
HamidReza <delshad.swdn@gmail.com>
2023-02-18 16:08:20 +0100

    create server.ts and app.ts

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,1 +1,9 @@
 // server here
+import Express from 'express'
+
+const app = Express()
+
+app.use(Express.json())
+
+
+export default app
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,1 +1,19 @@
 // connect database here
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import app from "./app";
+
+dotenv.config();
+mongoose.set("strictQuery", false);
+const port = 8000;
+mongoose
+  .connect(process.env.MONGODB_URL as string)
+  .then(() => {
+    app.listen(port, () => console.log(`The server is runnig on port ${port}`));
+  })
+  .catch((error: Error) => {
+    console.log(
+      `MongoDB connectin error. Please make sure MongoDB is running. ${error}`
+    );
+    process.exit(1);
+  });
