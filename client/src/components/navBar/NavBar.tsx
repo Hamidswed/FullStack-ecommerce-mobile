1c149dd9e93263d9c1f1cd028f4c95549efaf61c
HamidReza <delshad.swdn@gmail.com>
2023-02-20 00:07:05 +0100

    add navbar

--- client/src/components/navBar/NavBar.tsx
+++ client/src/components/navBar/NavBar.tsx
@@ -1,5 +1,35 @@
-import React from "react";
+import "./navBar.css";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
+import { IconButton } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 export default function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <div className="navbar">
+      <div className="navbar-logo">
+        <Logo />
+      </div>
+      <div className="navbar-menu">
+        <IconButton>
+          <HomeIcon />
+        </IconButton>
+        <IconButton>
+          <FormatListBulletedIcon />
+        </IconButton>
+        <IconButton>
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton>
+          <ShoppingCartIcon />
+        </IconButton>
+        <IconButton>
+          <AccountCircleIcon />
+        </IconButton>
+      </div>
+    </div>
+  );
 }
